4afdaeab7176e84915eb2972367263713ebf3cd7
DarthViderFinance <darthviderfinance@protonmail.com>
2021-03-07 02:59:51 +0100

    first commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,74 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.darthvider.finance',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.darthvider.finance/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token',
+      },
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/DarthViderFinance/",
+      },
+      {
+        label: "Blog",
+        href: "https://medium.com/@DarthViderfinance",
+      },
+    ],
+  },
+]
+
+export default config
---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0xa6477c795dA4B68813EA167660145464D89e6646',
+    97: '',
+  },
+  masterChef: {
+    56: '0x0a6EC5B5E431c3Cbf37674ff8D5bD76634639f1b',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0xA0b5940223Ee109d43Fbcd2B5f850d8000935790',
+    97: '',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
